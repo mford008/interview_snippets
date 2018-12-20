89b8960dd1172d070ffb684db4931cdbd04f76d4
Maddy Ford <mrhf008@gmail.com>
2018-12-20 11:10:32 -0800

    adding comments for hackerrank questions

--- interview_questions/diagonal_difference.py
+++ interview_questions/diagonal_difference.py
@@ -1,3 +1,6 @@
+# Hackerrank problem objective: find the absolute value of the
+# difference between the two diagonal sums in a matrix
+# Matrix defined here
 arr = [
     [1, 2, 3],
     [4, 5, 6],
@@ -6,12 +9,16 @@ arr = [
 
 
 def diagonalDifference(arr):
+    # Set variables for each diagonal sum
     d1 = 0
     d2 = 0
     n = len(arr)
+    # Loop through each value
     for i in range(n):
+        # Picking up array values where each equals its position in the larger array
         d1 += arr[i][i]
         d2 += arr[i][n-1-i]
+        # Get absolute difference
         diff = abs(d1 - d2)
     return diff
 
--- interview_questions/repeated_string.py
+++ interview_questions/repeated_string.py
@@ -2,11 +2,16 @@
 s = 'aba'
 
 def repeatedString(s, n):
+    # Set variable for length of string
     l = len(s)
+    # Number of how many times the string appears in n
     multiples = n//l
+    # Count how many times a appears in string
     substring_count = s.count('a')
+    # Preliminary count of times a appears
     result = substring_count * multiples
+    # Add number of times a appears in the remaining string letters that make up n
     result += s[:(n % l)].count('a')
-    print(result)
+    return result
 
 repeatedString(s, 10)
