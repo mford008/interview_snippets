2b8e460e636f3345713f47a60fae39120eaef9df
Maddy Ford <mrhf008@gmail.com>
2018-12-23 15:20:02 -0800

    added comments

--- interview_questions/staircase.py
+++ interview_questions/staircase.py
@@ -1,8 +1,11 @@
+# Set equal to test value
 n = 6
 
 
 def staircase(n):
+    # use range of 1 to n because we don't want a blank line on top
     for each in range(1, n + 1):
+        # use single print statement to print number of spaces plus hash
         print(' ' * (n - each) + '#' * each)
 
 
